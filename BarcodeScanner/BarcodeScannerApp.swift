132af75ca482fa082548c9af580b6c599982f28d
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2023-12-18 16:48:23 +0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Yong on 18/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
